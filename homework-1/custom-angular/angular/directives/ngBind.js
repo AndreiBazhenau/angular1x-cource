c811c098e5352d6273b283c5ec7b9a6574373aee
AndreiBazhenau <ab@ikiss.biz>
2016-10-03 18:32:09 +0300

    homework-1: bonus task

--- homework-1/custom-angular/angular/directives/ngBind.js
+++ homework-1/custom-angular/angular/directives/ngBind.js
@@ -1,7 +1,7 @@
 var ngBind = function() {
     return {
         compile: function ($scope, $element, $attr) {
-            $scope.$watch($attr.ngBind, function (value) {
+            $scope.$watch($attr.ngbind, function (value) {
                 $element.textContent = value;
             });
         }
